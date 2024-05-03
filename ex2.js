a99b9e325f1aaae0db3136a15354d53e2b2e107d
PATRICK MARIANO DE GODOY <45122325839@info.local>
2024-05-03 15:18:54 -0300

    feat: Exercício 2 Feito

---
+++ ex2.js
@@ -0,0 +1,28 @@
+// Exercício 2: Verificação de dia da semana
+//Escreva um programa que solicita ao usuário um número de 1 a 7 representando um dia da semana e exibe o nome correspondente a esse dia. Considere que 1 representa segunda-feira, 2 representa terça-feira e assim por diante.
+let dia = prompt("Insira o número do dia: ")
+
+switch (dia) {
+    case "1": alert("Hoje é Segunda-Feira")
+    break
+
+    case "2": alert("Hoje é Terça-Feira")
+    break
+
+    case "3": alert("Hoje é Quarta-Feira")
+    break
+
+    case "4": alert("Hoje é Quinta-Feira")
+    break
+
+    case "5": alert("Hoje é Sexta-Feira")
+    break
+
+    case "6": alert("Hoje é Sábado")
+    break
+
+    case "7": alert("Hoje é Domingo")
+    break
+
+}
+
